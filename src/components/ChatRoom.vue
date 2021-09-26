30bcfc9122ef0e40791910506cb822590cbb713f
sqmch <sqmchh@gmail.com>
2021-09-26 06:33:14 +0100

    Added basic design to Home page + misc tweaks

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="homebg flex flex-center">
     <q-input v-model="username" label="Standard" />
     <q-input v-model="message_text" label="Standard" />
 
@@ -13,40 +13,34 @@
   </q-page>
 </template>
 
-<style>
-</style>
-
 <script>
-import { ref } from "vue";
+export default defineComponent({
+  name: "ChatRoom",
+});
+</script>
+
+<script setup>
+import { ref, defineComponent } from "vue";
 import store from "../store";
 
-export default {
-  name: "ChatRoom",
-  setup() {
-    const messages = ref([]);
-    let message_text = ref("");
-    const username = store.state.username;
-    const ws = new WebSocket(`ws://localhost:8000/ws/${store.state.username}`);
+const messages = ref([]);
+let message_text = ref("");
+const username = store.state.username;
+const ws = new WebSocket(`ws://localhost:8000/ws/${store.state.username}`);
 
-    ws.onmessage = function (event) {
-      messages.value.push(event.data);
-      console.log("onmessage event.data: ", messages.value);
-      console.log("messages: ", messages.value);
-    };
-    function addMessage() {}
-    function sendMessage() {
-      console.log("sendMessage ran with text: ", message_text.value);
-      ws.send(message_text.value);
-      message_text.value = "";
-    }
-    return {
-      leftDrawerOpen: ref(false),
-      username,
-      message_text,
-      sendMessage,
-      addMessage,
-      messages,
-    };
-  },
+ws.onmessage = function (event) {
+  messages.value.push(event.data);
+  console.log("onmessage event.data: ", messages.value);
+  console.log("messages: ", messages.value);
 };
+function sendMessage() {
+  console.log("sendMessage ran with text: ", message_text.value);
+  ws.send(message_text.value);
+  message_text.value = "";
+}
 </script>
+<style lang="sass">
+.homebg
+  background: rgb(255, 227, 227)
+  background: radial-gradient(circle, rgba(231, 224, 201,0.8239670868347339) -25%, rgba(17, 50, 77,0.8127626050420168) 150%, rgba(17, 50, 77,0.8) 100%)
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,43 +1,61 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-input v-model="username" label="Standard" />
-
-    <q-btn
-      :to="'chatroom'"
-      @click="setUser()"
-      color="white"
-      text-color="black"
-      label="Standard"
-    />
+  <q-page class="homebg flex flex-center">
+    <div class="usernamebg row" style="">
+      <div class="col-8">
+        <q-input outlined v-model="username" label="Your username" />
+      </div>
+      <div class="col-4">
+        <div class="">
+          <q-btn
+            large
+            class="enterButton"
+            :to="'chatroom'"
+            @click="setUser()"
+            label="Enter"
+            :ripple="{ color: 'yellow' }"
+          />
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<style>
-</style>
-
 <script>
-import { ref } from "vue";
-import { mapMutations } from "vuex";
-import store from "../store";
-
-export default {
+export default defineComponent({
   name: "ChatRoom",
-
-  setup() {
-    let username = ref("");
-    function setUser() {
-      store.commit("setUserName", username.value);
-      console.log(store.state.username);
-    }
-    return {
-      username,
-      setUser,
-    };
-  },
   methods: {
     ...mapMutations([
       "setUserName", // map `this.increment()` to `this.$store.commit('increment')`'incrementBy' // map `this.incrementBy(amount)` to `this.$store.commit('incrementBy', amount)`
     ]),
   },
-};
+});
 </script>
+
+<script setup>
+import { ref, defineComponent } from "vue";
+import { mapMutations } from "vuex";
+import store from "../store";
+
+let username = ref("");
+function setUser() {
+  store.commit("setUserName", username.value);
+  console.log(store.state.username);
+}
+</script>
+<style lang="sass">
+.homebg
+  background: rgb(255, 227, 227)
+  background: radial-gradient(circle, rgba(231, 224, 201,0.8239670868347339) -25%, rgba(17, 50, 77,0.8127626050420168) 150%, rgba(17, 50, 77,0.8) 100%)
+
+.usernamebg
+  padding: 25px
+  width: 25em
+
+.enterButton
+  margin-left: 1.5em
+  margin-top: 0.5em
+  background: #6B7AA1
+  color: #FFE3E3
+  font-size: 1.1em
+  letter-spacing: 0.1rem
+</style>
